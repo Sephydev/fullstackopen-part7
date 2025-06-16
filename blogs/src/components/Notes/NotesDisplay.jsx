d25d8c2eac176d1e826c84fd126b4da7fea5da5b
Sephydev <nicolas.bussiere@outlook.fr>
2025-06-16 19:54:34 +0200

    Remove blogs because wrong starting code

--- blogs/src/components/Notes/NotesDisplay.jsx
+++
@@ -1,9 +0,0 @@
-const NotesDisplay = ({notes}) => {
-    return (
-        <>
-            {notes.map(n => <p key={n.id}>{n.content}</p>)}
-        </>
-    )
-}
-
-export default NotesDisplay
--- blogs/src/components/Persons/PersonsDisplay.jsx
+++
@@ -1,9 +0,0 @@
-const PersonsDisplay = ({persons}) => {
-    return (
-        <>
-            {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
-        </>
-    )
-}
-
-export default PersonsDisplay
