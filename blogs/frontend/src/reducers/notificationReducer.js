f370fa7dd047816c23c9e71ec90916d6a9891880
Sephydev <nicolas.bussiere@outlook.fr>
2025-06-17 16:59:05 +0200

    Refactor Notification feature with redux

---
+++ blogs/frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: "notification",
+  initialState: null,
+  reducers: {
+    setNotificationAction(state, action) {
+      return action.payload;
+    },
+    removeNotification() {
+      return null;
+    },
+  },
+});
+
+export const setNotification = (text) => {
+  return (dispatch) => {
+    dispatch(setNotificationAction(text));
+    setTimeout(() => {
+      dispatch(removeNotification());
+    }, 5000);
+  };
+};
+
+export const { setNotificationAction, removeNotification } =
+  notificationSlice.actions;
+export default notificationSlice.reducer;
---
+++ blogs/frontend/src/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import notificationReducer from './reducers/notificationReducer'
+
+const creatingStore = () => {
+  return configureStore({
+    reducer: {
+      notification: notificationReducer
+    }
+  })
+}
+
+export default creatingStore
